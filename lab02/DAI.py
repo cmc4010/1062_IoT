ce1671626186d2d1baa2b401c0ef22f20a4cb83c
Chao-Ming Chang <cmc4010@gmail.com>
2018-04-07 22:47:44 +0800

    completed lab02

--- lab02/DAI.py
+++ lab02/DAI.py
@@ -1,20 +1,30 @@
 import time, DAN, requests, random
 
 # ServerIP = '140.113.199.202' #Change to your IoTtalk IP or None for autoSearching
-ServerIP = '140.113.199.246' # https://test.iottalk.tw
-Reg_addr=None #None # if None, Reg_addr = MAC address
+ServerIP = '140.113.199.199' # https://test.iottalk.tw
+Reg_addr = None #None # if None, Reg_addr = MAC address
 
 DAN.profile['dm_name']='Dummy_Device'
 DAN.profile['df_list']=['Dummy_Sensor', 'Dummy_Control','OneParameter','ThreeParameter']
 DAN.profile['d_name']= None # None for autoNaming
 DAN.device_registration_with_retry(ServerIP, Reg_addr)
 
+printFlag = 0
+
+# Start Device
 while True:
     try:
     #Pull data from a device feature called "Dummy_Control"
-        value1=DAN.pull('Dummy_Control')
+        value1 = DAN.pull('Dummy_Control') # array
+
         if value1 != None:
-            print (value1[0])
+            if value1[0] == 1:
+                printFlag = 1
+            else:
+                printFlag = 0
+
+        if printFlag == 1:
+            print (random.randint(1,101)) # between 1 and 100
 
     #Push data to a device feature called "Dummy_Sensor"
         # value2=random.uniform(1, 10)
